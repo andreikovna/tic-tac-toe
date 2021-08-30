3ca6fa52217bfbe0e120544a9e365810855db6e5
andreikovna <d.andreikovna@gmail.com>
2021-08-30 16:11:49 +0300

    add solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,75 @@
 class TicTacToe {
     constructor() {
-
+        
+        this.currentPlayer = 'x';
+        this.playField = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
+        ];
     }
+    
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.playField[rowIndex][columnIndex] != null) {
+            return;
+        }
+        this.playField[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+        this.currentPlayer = this.currentPlayer == 'x' ? 'o' : 'x';
     }
 
     isFinished() {
-
+        if (this.getWinner() || this.isDraw()) {
+            return true;
+        }
+        return false;
     }
 
     getWinner() {
-
+        if (this.playField[0][0] === this.playField[0][1] && this.playField[0][1] === this.playField[0][2]) {
+            return this.playField[0][0];
+        }
+        if (this.playField[1][0] === this.playField[1][1] && this.playField[1][1] === this.playField[1][2]) {
+            return this.playField[1][0];
+        }
+        if (this.playField[2][0] === this.playField[2][1] && this.playField[2][1] === this.playField[2][2]) {
+            return this.playField[2][0];
+        }
+        if (this.playField[0][0] === this.playField[1][0] && this.playField[1][0] === this.playField[2][0]) {
+            return this.playField[0][0];
+        }
+        if (this.playField[0][1] === this.playField[1][1] && this.playField[1][1] === this.playField[2][1]) {
+            return this.playField[0][1];
+        }
+        if (this.playField[0][2] === this.playField[1][2] && this.playField[1][2] === this.playField[2][2]) {
+            return this.playField[0][2];
+        }
+        if (this.playField[0][0] === this.playField[1][1] && this.playField[1][1] === this.playField[2][2]) {
+            return this.playField[0][0];
+        }
+        if (this.playField[0][2] === this.playField[1][1] && this.playField[1][1] === this.playField[2][0]) {
+            return this.playField[0][2];
+        }
+        return null;
     }
 
     noMoreTurns() {
-
+        return this.playField.every(el => el.indexOf(null) == -1);
     }
 
     isDraw() {
-
+        if (this.noMoreTurns() && !this.getWinner()) {
+            return true;
+        }
+        return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.playField[rowIndex][colIndex];
     }
 }
 
